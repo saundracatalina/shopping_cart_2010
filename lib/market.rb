43c8c22961c2779c6bf01a58304ac0d28d751313
Saundra <saundracatalina@gmail.com>
2020-11-11 09:34:59 -0700

    Add test_can_list_vendor_attributes

--- lib/market.rb
+++ lib/market.rb
@@ -8,6 +8,18 @@ class Market
   end
 
   def add_vendor(vendor)
-    @vendors << vendor 
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor if vendor.inventory.include?(item)
+    end
   end
 end
