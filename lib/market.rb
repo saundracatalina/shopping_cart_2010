13a61f237ef0e1f1fd15dbf7bc1220bec80917c8
Saundra <saundracatalina@gmail.com>
2020-11-11 10:10:17 -0700

    Add test_can_report_total_inventory

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,29 @@ class Market
       vendor if vendor.inventory.include?(item)
     end
   end
+
+  def total_inventory
+    inventory = {}
+    @vendors.each do |vendor|
+      items_at_market.each do |item|
+        inventory[item] = {
+          quantity: total_quantity_of_item(item),
+          vendors: vendors_that_sell(item)
+        }
+      end
+    end
+    inventory
+  end
+
+  def items_at_market
+    @vendors.flat_map do |vendor|
+      vendor.inventory.keys
+    end.uniq
+  end
+
+  def total_quantity_of_item(item)
+    @vendors.sum do |vendor|
+      vendor.inventory[item]
+    end
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -59,4 +59,46 @@ class MarketTest < Minitest::Test
     assert_equal [vendor1, vendor3], market.vendors_that_sell(item1)
     assert_equal [vendor2], market.vendors_that_sell(item4)
   end
+
+  def test_can_report_total_inventory
+    market = Market.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: "Peach", price: "$0.75"})
+    item2 = Item.new({name: "Tomato", price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    vendor3.stock(item3, 10)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expected_hash = {
+                  item1 => {
+                            quantity: 100,
+                            vendors: [vendor1, vendor3]
+                            },
+                  item2 => {
+                            quantity: 7,
+                            vendors: [vendor1]
+                            },
+                  item4 => {
+                            quantity: 50,
+                            vendors: [vendor2]
+                            },
+                  item3 => {
+                            quantity: 35,
+                            vendors: [vendor2, vendor3]
+                            },
+                      }
+    assert_equal expected_hash, market.total_inventory
+    assert_equal [item1, item2, item4, item3], market.items_at_market
+    assert_equal 100, market.total_quantity_of_item(item1)
+  end
 end
