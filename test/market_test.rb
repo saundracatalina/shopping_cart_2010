32bce42e4a0ade0debb7910e6772b24f61059780
Saundra <saundracatalina@gmail.com>
2020-11-11 09:29:40 -0700

    Add test_can_add_and_list_vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,7 +9,29 @@ class MarketTest < Minitest::Test
     market = Market.new("South Pearl Street Farmers Market")
 
     assert_instance_of Market, market
-    assert_equal "South Pearl Street Farmers Market", market.name 
+    assert_equal "South Pearl Street Farmers Market", market.name
     assert_equal [], market.vendors
+  end
+
+  def test_can_add_and_list_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expected_array = [vendor1, vendor2, vendor3]
+    assert_equal expected_array, market.vendors
   end
 end
